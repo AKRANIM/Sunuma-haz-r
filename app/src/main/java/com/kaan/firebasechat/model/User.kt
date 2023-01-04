42117c572132c8cd518394022127c2a6fa4629c2
KaanC3lik <100537211+KaanC3lik@users.noreply.github.com>
2023-01-04 12:12:47 +0300

    Tamamlandı

---
+++ app/src/main/java/com/kaan/firebasechat/model/User.kt
@@ -0,0 +1,10 @@
+package com.kaan.firebasechat.model
+
+data class User(val userId:String = "",
+                val userName:String = "",
+                val userImage:String = "",
+                val userAkran:String = "",
+                val schoolId:String = "",
+                val department:String = "",
+                val section:String = "",
+                )
